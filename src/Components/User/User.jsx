d312e1469ea787c720113753a61acf5bb9dc1f3f
piasmajumdar <piasmajumdar@gmail.com>
2026-04-18 02:36:14 +0530

    Add user navigation and toggle for username display in User component

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './User.css'
-import { Link } from 'react-router';
+import { Link, Navigate } from 'react-router';
 
-const User = ({user}) => {
+const User = ({ user }) => {
+    const [showUserName, setShowUserName] = useState(false)
+    const [visitHome, setVisitHome] = useState(false);
 
     // console.log(user)
+
+    if (visitHome) {
+        return <Navigate to={'/'}></Navigate>
+    }
+
 
     return (
         <div className='user-card'>
@@ -12,6 +19,13 @@ const User = ({user}) => {
             <p>Email: {user.email}</p>
             <p>Phone: {user.phone}</p>
             <Link to={`/users/${user.id}`}>Show Details</Link>
+            <button onClick={() => setShowUserName(!showUserName)}>{showUserName ? 'Hide username' : 'Show username'}</button>
+            {
+                showUserName && <div>
+                    <p>Username: {user.username}</p>
+                </div>
+            }
+            <button onClick={() => setVisitHome(true)}>Visit Home</button>
         </div>
     );
 };
